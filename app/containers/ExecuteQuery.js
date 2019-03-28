12a4b71882acd401db5c85d97ae2ba94db2c9ef5
Amit Sengar <amitsengar@yahoo.com>
2019-03-28 15:31:06 +0530

    app name changed

--- app/containers/ExecuteQuery.js
+++ app/containers/ExecuteQuery.js
@@ -1,11 +1,5 @@
 import React, { Component } from 'react';
-import {
-  TextField,
-  Button,
-  Paper,
-  CircularProgress,
-  Fab
-} from '@material-ui/core';
+import { TextField, Button, Paper, CircularProgress, Fab } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { AddQueryStatement } from './../actions/actioncreater';
@@ -17,142 +11,139 @@ import APITransport from './../api/apiTransport';
 import GetRecordsAPI from './../api/getRecordsAPI';
 
 class ExecuteQuery extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isProgress: false };
-  }
-  executeSelect = statement => {
-    let api = new GetRecordsAPI(statement, 3000);
-    this.props.APITransport(api);
-    this.setState({ isProgress: false })
-    // CassandraAPICalls.fireGetQuery(statement)
-    // .then(
-    //     resp => {
-    //         if (resp.status === 200) {
-    //             this.props.showNotification(true, "Record fetched Successfully", 'success')
-    //         }
-    //         else {
-    //             this.props.showNotification(true, "No Record Found", 'warning')
-    //         }
-    //         if (resp.data !== undefined) {
-    //             this.props.AddResult(resp.data)
-    //         }
-    //         this.setState({ isProgress: false })
-    //     }
-    // )
-    // .catch(error => {
-    //     console.log(error)
-    //     this.props.AddResult([])
-    //     this.props.showNotification(true, error.response.statusText, 'error')
-    //     this.setState({ isProgress: false })
-    // })
-  };
+	constructor(props) {
+		super(props);
+		this.state = { isProgress: false };
+	}
+	executeSelect = (statement) => {
+		let api = new GetRecordsAPI(statement, 3000);
+		this.props.APITransport(api);
+		this.setState({ isProgress: false });
+		// CassandraAPICalls.fireGetQuery(statement)
+		// .then(
+		//     resp => {
+		//         if (resp.status === 200) {
+		//             this.props.showNotification(true, "Record fetched Successfully", 'success')
+		//         }
+		//         else {
+		//             this.props.showNotification(true, "No Record Found", 'warning')
+		//         }
+		//         if (resp.data !== undefined) {
+		//             this.props.AddResult(resp.data)
+		//         }
+		//         this.setState({ isProgress: false })
+		//     }
+		// )
+		// .catch(error => {
+		//     console.log(error)
+		//     this.props.AddResult([])
+		//     this.props.showNotification(true, error.response.statusText, 'error')
+		//     this.setState({ isProgress: false })
+		// })
+	};
 
-  executeDelete = statement => {
-    CassandraAPICalls.fireQuery(statement)
-      .then(resp => {
-        if (resp.status === 200) {
-          this.props.showNotification(
-            true,
-            'Record Deleted Successfully',
-            'success'
-          );
-        } else {
-          this.props.showNotification(true, 'No Record Found', 'warning');
-        }
-        if (resp.data !== undefined) {
-          this.props.AddResult(resp.data);
-        }
-        this.setState({ isProgress: false });
-      })
-      .catch(error => {
-        console.log(error);
-        this.props.AddResult([]);
-        this.props.showNotification(
-          true,
-          'Delete Query Failed ' + error,
-          'error'
-        );
-        this.setState({ isProgress: false });
-      });
-  };
+	executeDelete = (statement) => {
+		let api = new GetRecordsAPI(statement, 3000);
+		this.props.APITransport(api);
+		this.setState({ isProgress: false });
 
-  handleOnClick = () => {
-    const { statement } = this.props;
+		// CassandraAPICalls.fireQuery(statement)
+		//   .then(resp => {
+		//     if (resp.status === 200) {
+		this.props.showNotification(true, 'Record Deleted Successfully', 'success');
+		//   } else {
+		//     this.props.showNotification(true, 'No Record Found', 'warning');
+		//   }
+		//   if (resp.data !== undefined) {
+		//     this.props.AddResult(resp.data);
+		//   }
+		//   this.setState({ isProgress: false });
+		// })
+		// .catch(error => {
+		//   console.log(error);
+		//   this.props.AddResult([]);
+		//   this.props.showNotification(
+		//     true,
+		//     'Delete Query Failed ' + error,
+		//     'error'
+		//   );
+		//   this.setState({ isProgress: false });
+		// });
+	};
 
-    if (statement === '') {
-      this.props.showNotification(
-        true,
-        'Select keyspace, column family and columns for this operation',
-        'warning'
-      );
-    } else {
-      this.setState({ isProgress: true });
-      if (statement.includes('DELETE') || statement.includes('delete')) {
-        this.executeDelete(statement);
-      } else {
-        this.executeSelect(statement);
-      }
-    }
-  };
+	handleOnClick = () => {
+		const { statement } = this.props;
 
-  render() {
-    console.log('render() : ExecuteQuery');
-    return (
-      <div>
-        <Notification />
-        <TextField
-          id="query"
-          label="query"
-          multiline
-          value={this.props.statement}
-          onChange={event => this.props.AddQueryStatement(event.target.value)}
-          style={{ width: '99%', margin: '0.5%' }}
-        />
-        <Paper style={{ padding: '20px 20px' }}>
-          <Fab
-            onClick={() => this.handleOnClick()}
-            variant="extended"
-            aria-label="execute"
-            style={{ background: '#424242' }}
-          >
-            Execute
-          </Fab>
-        </Paper>
-        {this.state.isProgress && (
-          <CircularProgress
-            size={60}
-            style={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%'
-            }}
-          />
-        )}
-      </div>
-    );
-  }
+		if (statement === '') {
+			this.props.showNotification(
+				true,
+				'Select keyspace, column family and columns for this operation',
+				'warning'
+			);
+		} else {
+			this.setState({ isProgress: true });
+			if (statement.includes('DELETE') || statement.includes('delete')) {
+				this.executeDelete(statement);
+			} else {
+				this.executeSelect(statement);
+			}
+		}
+	};
+
+	render() {
+		console.log('render() : ExecuteQuery');
+		return (
+			<div>
+				<Notification />
+				<TextField
+					id="query"
+					label="query"
+					multiline
+					value={this.props.statement}
+					onChange={(event) => this.props.AddQueryStatement(event.target.value)}
+					style={{ width: '99%', margin: '0.5%' }}
+				/>
+				<Paper style={{ padding: '20px 20px' }}>
+					<Fab
+						onClick={() => this.handleOnClick()}
+						variant="extended"
+						aria-label="execute"
+						style={{ background: '#424242' }}
+					>
+						Execute
+					</Fab>
+				</Paper>
+				{this.state.isProgress && (
+					<CircularProgress
+						size={60}
+						style={{
+							position: 'absolute',
+							top: '50%',
+							left: '50%'
+						}}
+					/>
+				)}
+			</div>
+		);
+	}
 }
 
-const mapStateToProps = state => {
-  return {
-    statement: state.queryReducer.statement
-  };
+const mapStateToProps = (state) => {
+	return {
+		statement: state.queryReducer.statement
+	};
 };
 
-const matchDispatchToProps = dispatch => {
-  return bindActionCreators(
-    {
-      APITransport: APITransport,
-      AddQueryStatement: AddQueryStatement,
-      AddResult: AddResult,
-      showNotification: showNotification
-    },
-    dispatch
-  );
+const matchDispatchToProps = (dispatch) => {
+	return bindActionCreators(
+		{
+			APITransport: APITransport,
+			AddQueryStatement: AddQueryStatement,
+			AddResult: AddResult,
+			showNotification: showNotification
+		},
+		dispatch
+	);
 };
 
-export default connect(
-  mapStateToProps,
-  matchDispatchToProps
-)(ExecuteQuery);
+export default connect(mapStateToProps, matchDispatchToProps)(ExecuteQuery);
