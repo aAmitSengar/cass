cce2336ed89f446f6241da4772eaa99c809361db
Amit Sengar <amitsengar@yahoo.com>
2019-04-10 12:44:11 +0530

    fixes

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -1,18 +1,22 @@
+/* eslint-disable flowtype/no-weak-types */
 // @flow
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { createHashHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import createRootReducer from '../reducers';
-import initialState from './../data/appstate/initialstate';
-
+import initialState from '../data/appstate/initialstate';
 
 const history = createHashHistory();
 const rootReducer = createRootReducer(history);
 const router = routerMiddleware(history);
 const enhancer = applyMiddleware(thunk, router);
 
-function configureStore(initialState1?:any) {
+// eslint-disable-next-line flowtype/no-weak-types
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line flowtype/no-weak-types
+// eslint-disable-next-line no-unused-vars
+function configureStore(initialState1?: any) {
   return createStore(rootReducer, initialState, enhancer);
 }
 
